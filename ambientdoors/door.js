5a2def25da1efae55f0fd3d4cb713b312a7e2e11
EndlesNights <58280840+EndlesNights@users.noreply.github.com>
2020-06-16 16:49:33 -0400

    Reworking door.js

--- ambientdoors/door.js
+++ ambientdoors/door.js
@@ -1,44 +1,74 @@
-Hooks.on("updateWall", (scene, wallData) => {
-		
-	if(wallData.door == 0 || wallData.ds == wallData.flags.ambientdoors.lastState)
+var superwall_id = '';
+var mod = "ambientdoors"; // our mod scope & name
+
+let ambDoorData = {
+	openPath: "modules/ambientdoors/defaultSounds/DoorOpenSound.wav",
+	openLevel: 0.8,
+	closePath: "modules/ambientdoors/defaultSounds/DoorCloseSound.wav",
+	closeLevel: 0.8,
+	lockPath: "modules/ambientdoors/defaultSounds/DoorLockSound.wav",
+	lockLevel: 0.8,
+	unlockPath: "modules/ambientdoors/defaultSounds/DoorUnLockSound.wav",
+	unlockLevel: 0.8
+};
+
+Hooks.on("preUpdateWall", async (scene, object, updateData,userID) => {
+	
+	if(object.door == 0 || updateData.ds == null)
+	{
 		return;
-	
-	
-	var playpath = "";
-	
-	if(wallData.flags.ambientdoors.lastState == 2) // Door Unlocking
-	{
-		playpath = wallData.flags.ambientdoors.unLockPath;
-		wallData.flags.ambientdoors.lastState = 0;
 	}
-	else
+
+	let playpath = "";
+	let playVolume = 0.8;
+	
+	if(object.ds == 2) // Door Unlocking
 	{
-		if(wallData.ds == 0) //Door Close
-			playpath = wallData.flags.ambientdoors.closePath; 
-		if(wallData.ds == 1) //Door Open
-			playpath = wallData.flags.ambientdoors.openPath; 
-		if(wallData.ds == 2) // Door Lock
-			playpath = wallData.flags.ambientdoors.lockPath;
-			
-		wallData.flags.ambientdoors.lastState = wallData.ds;		
+		playpath = object.flags.ambientdoors.doorData.unlockPath;
+		playVolume = object.flags.ambientdoors.doorData.unlockLevel;
+	}
+	else if(updateData.ds == 0) //Door Close
+	{
+		playpath = object.flags.ambientdoors.doorData.closePath;
+		playVolume = object.flags.ambientdoors.doorData.closeLevel;
+	}
+	else if(updateData.ds == 1) //Door Open
+	{
+		playpath = object.flags.ambientdoors.doorData.openPath;
+		playVolume = object.flags.ambientdoors.doorData.openLevel;
+	}
+	else if(updateData.ds == 2) // Door Lock
+	{
+		playpath = object.flags.ambientdoors.doorData.lockPath;
+		playVolume = object.flags.ambientdoors.doorData.lockLevel;
 	}
 	
 	if(playpath != "" && playpath != null)
 	{
-		AudioHelper.play({src: playpath, volume: 0.8, autoplay: true, loop: false}, true);
+		AudioHelper.play({src: playpath, volume: playVolume, autoplay: true, loop: false}, true);
 	}
+});
+
+Hooks.on('createWall', async (scene,object,updateData,userID) => {
+	
+	console.log(userID);
+	
+	console.log(scene);
+	superwall_id = object._id;
+	wall = await canvas.walls.get(superwall_id);
+	wall.setFlag(mod,"doorData", ambDoorData);
+	wall.update(wall.data);
 });
 
 
 Hooks.on("renderWallConfig", (app, html, data) => {
-		
+
 	if(data.object.door == 0) // if it's not a door don't worry
 	{
 		app.setPosition({
 		height: 270,
 		width: 400
 		});
-		
 		return;
 	}
 	
@@ -47,36 +77,22 @@ Hooks.on("renderWallConfig", (app, html, data) => {
 		width: 520
 	});
 
-	var closeFlag = app.object.getFlag("ambientdoors", "closePath");
-	var openFlag = app.object.getFlag("ambientdoors", "openPath");
-	var lockFlag = app.object.getFlag("ambientdoors", "lockPath");
-	var unLockFlag = app.object.getFlag("ambientdoors", "unLockPath");
+	let thisDoor;
 	
-	if(app.object.getFlag("ambientdoors", "lastState") == null)
-	{		
-		app.object.setFlag("ambientdoors", "lastState", data.object.ds);
+	if(app.object.getFlag(mod, "doorData") == null)
+	{
+		app.object.setFlag(mod, "doorData", ambDoorData);
 	}
-	if(app.object.getFlag("ambientdoors", "closePath") == null)
-	{		
-		app.object.setFlag("ambientdoors", "closePath", "modules/ambientdoors/defaultSounds/DoorCloseSound.wav");
-		closeFlag = "modules/ambientdoors/defaultSounds/DoorCloseSound.wav";
+	else
+	{
+		thisDoor = app.object.getFlag(mod, "doorData");
 	}
-	if(app.object.getFlag("ambientdoors", "openPath") == null)
-	{		
-		app.object.setFlag("ambientdoors", "openPath", "modules/ambientdoors/defaultSounds/DoorOpenSound.wav");
-		openFlag = "modules/ambientdoors/defaultSounds/DoorOpenSound.wav";
-	}
-	if(app.object.getFlag("ambientdoors", "lockPath") == null)
-	{		
-		app.object.setFlag("ambientdoors", "lockPath", "modules/ambientdoors/defaultSounds/DoorLockSound.wav");
-		lockFlag = "modules/ambientdoors/defaultSounds/DoorLockSound.wav";
-	}
-	if(app.object.getFlag("ambientdoors", "unLockPath") == null)
-	{		
-		app.object.setFlag("ambientdoors", "unLockPath", "modules/ambientdoors/defaultSounds/DoorUnlockSound.wav");
-		unLockFlag = "modules/ambientdoors/defaultSounds/DoorUnlockSound.wav";
-	}
-
+	
+	var closeFlag = thisDoor.closePath;
+	var openFlag = thisDoor.openPath;
+	var lockFlag = thisDoor.lockPath;
+	var unLockFlag = thisDoor.unlockPath;
+	
 	
 	const message =`
 	<div class="form-group">
@@ -87,55 +103,52 @@ Hooks.on("renderWallConfig", (app, html, data) => {
     <div class="form-group">
 		<label>Door Close</label>
 		<div class="form-fields">
-			<button type="button" class="file-picker" data-type="sound" data-target="flags.ambientdoors.closePath" title="Browse Files" tabindex="-1">
+			<button type="button" class="file-picker" data-type="sound" data-target="flags.ambientdoors.doorData.closePath" title="Browse Files" tabindex="-1">
 				<i class="fas fa-file-import fa-fw"></i>
 			</button>
-			<input class="sound" type="text" name="flags.ambientdoors.closePath" value="${closeFlag ? closeFlag : ``}" placeholder="Door Close Sound Path" data-dtype="String" />
+			<input class="sound" type="text" name="flags.ambientdoors.doorData.closePath" value="${closeFlag ? closeFlag : ``}" placeholder="Door Close Sound Path" data-dtype="String" />
 		</div>
 	</div>
 	
     <div class="form-group">
 		<label>Door Open</label>
 		<div class="form-fields">
-			<button type="button" class="file-picker" data-type="sound" data-target="flags.ambientdoors.openPath" title="Browse Files" tabindex="-1">
+			<button type="button" class="file-picker" data-type="sound" data-target="flags.ambientdoors.doorData.openPath" title="Browse Files" tabindex="-1">
 				<i class="fas fa-file-import fa-fw"></i>
 			</button>
-			<input class="sound" type="text" name="flags.ambientdoors.openPath" value="${openFlag ? openFlag : ``}" placeholder="Door Open Sound Path" data-dtype="String" />
+			<input class="sound" type="text" name="flags.ambientdoors.doorData.openPath" value="${openFlag ? openFlag : ``}" placeholder="Door Open Sound Path" data-dtype="String" />
 		</div>
 	</div>
 	
 	<div class="form-group">
 		<label>Door Lock</label>
 		<div class="form-fields">
-			<button type="button" class="file-picker" data-type="sound" data-target="flags.ambientdoors.lockPath" title="Browse Files" tabindex="-1">
+			<button type="button" class="file-picker" data-type="sound" data-target="flags.ambientdoors.doorData.lockPath" title="Browse Files" tabindex="-1">
 				<i class="fas fa-file-import fa-fw"></i>
 			</button>
-			<input class="sound" type="text" name="flags.ambientdoors.lockPath" value="${lockFlag ? lockFlag : ``}" placeholder="Door Lock Sound Path" data-dtype="String" />
+			<input class="sound" type="text" name="flags.ambientdoors.doorData.lockPath" value="${lockFlag ? lockFlag : ``}" placeholder="Door Lock Sound Path" data-dtype="String" />
 		</div>
 	</div>
 	
 	<div class="form-group">
 		<label>Door Unlock</label>
 		<div class="form-fields">
-			<button type="button" class="file-picker" data-type="sound" data-target="flags.ambientdoors.unLockPath" title="Browse Files" tabindex="-1">
+			<button type="button" class="file-picker" data-type="sound" data-target="flags.ambientdoors.doorData.unlockPath" title="Browse Files" tabindex="-1">
 				<i class="fas fa-file-import fa-fw"></i>
 			</button>
-			<input class="sound" type="text" name="flags.ambientdoors.unLockPath" value="${unLockFlag ? unLockFlag : ``}" placeholder="Door Unlock Sound Path" data-dtype="String" />
+			<input class="sound" type="text" name="flags.ambientdoors.doorData.unlockPath" value="${unLockFlag ? unLockFlag : ``}" placeholder="Door Unlock Sound Path" data-dtype="String" />
 		</div>
 	</div>
 	`
-    
+	
 	html.find(".form-group").last().after(message);
-	const button = html.find('button[data-target="flags.ambientdoors.closePath"]')[0];
-	const button2 = html.find('button[data-target="flags.ambientdoors.openPath"]')[0];
-	const button3 = html.find('button[data-target="flags.ambientdoors.lockPath"]')[0];
-	const button4 = html.find('button[data-target="flags.ambientdoors.unLockPath"]')[0];
+	const button = html.find('button[data-target="flags.ambientdoors.doorData.closePath"]')[0];
+	const button2 = html.find('button[data-target="flags.ambientdoors.doorData.openPath"]')[0];
+	const button3 = html.find('button[data-target="flags.ambientdoors.doorData.lockPath"]')[0];
+	const button4 = html.find('button[data-target="flags.ambientdoors.doorData.unlockPath"]')[0];
 	
 	app._activateFilePicker(button);
 	app._activateFilePicker(button2);
 	app._activateFilePicker(button3);
 	app._activateFilePicker(button4);
 });
-
-
-
